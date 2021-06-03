70ce0f241467ca210bd95327fc50ceebb933b38b
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-06-03 17:15:36 -0300

    chore: setup husky and commitizen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
