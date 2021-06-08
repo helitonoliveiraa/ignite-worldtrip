40f63e9bd9498b4412b716ee771642daee578c4f
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-06-08 17:08:51 -0300

    chore: import swiper styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+
+import 'swiper/swiper.min.css';
+import 'swiper/components/navigation/navigation.min.css';
+import 'swiper/components/pagination/pagination.min.css';
+
 import { theme } from '../styles/theme';
 import { Header } from '../components/Header';
 
